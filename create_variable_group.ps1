96620b741188bdf63f884eedb6d07fe6f32839b5
joshua781467 <67278486+joshua781467@users.noreply.github.com>
2021-06-21 09:08:07 +0530

    Create create_variable_group.ps1

---
+++ create_variable_group.ps1
@@ -0,0 +1,13 @@
+param (
+        [string]$workspace,
+        [string]$path,
+        [string]$bearerToken,
+        [string]$organization,
+        [string]$project
+    )
+
+Write-Output $workspace
+Write-Output $path
+Write-Output $bearerToken
+Write-Output $organization
+Write-Output $project
